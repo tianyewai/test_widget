2de782b02adfc13a14ace156a71e80d9a28e0e15
xihui <xihui@huimeifeng2012.com>
2020-05-31 19:42:12 +0800

    Share

---
+++ fileshare/ShareViewController.h
@@ -0,0 +1,14 @@
+//
+//  ShareViewController.h
+//  fileshare
+//
+//  Created by xihui on 2020/5/31.
+//  Copyright © 2020 xihui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Social/Social.h>
+
+@interface ShareViewController : SLComposeServiceViewController
+
+@end
